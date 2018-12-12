1eb9f20ecce2cd45521093e031f860d3353c1664
unknown <a.atkociunaite@gmail.com>
2018-12-12 21:11:17 +0200

    Skaiciuoti darbo trukme

---
+++ atlikimo_laikas.py
@@ -0,0 +1,33 @@
+import datetime
+import sys
+
+arguments = sys.argv[1:]
+
+first_argument = arguments[0]
+now = str(datetime.datetime.now())
+
+if first_argument == 'pradeti':
+    f = open('task.txt', mode = 'w')
+    f.write(now)
+    f.close()
+
+if first_argument == 'pabaigti':
+    f = open('task.txt')
+    pradeti = f.read()
+    print(pradeti)
+    print(now)
+    trukme = None
+    f.close()
+    now = datetime.datetime.strptime(now, '%Y-%m-%d %H:%M:%S.%f')
+    pradeti = datetime.datetime.strptime(pradeti, '%Y-%m-%d %H:%M:%S.%f')
+    print(now - pradeti)
+    
+
+
+
+
+
+
+
+
+
