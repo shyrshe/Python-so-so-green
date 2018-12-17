437cbdb49825f3c436c642611e13ecc729adc3f5
unknown <a.atkociunaite@gmail.com>
2018-12-17 21:12:25 +0200

    Fix #1

--- atlikimo_laikas.py
+++ atlikimo_laikas.py
@@ -1,18 +1,20 @@
 import datetime
 import sys
-
+ 
 arguments = sys.argv[1:]
 
 first_argument = arguments[0]
+second_argument = arguments[1]
+
 now = str(datetime.datetime.now())
 
 if first_argument == 'pradeti':
-    f = open('task.txt', mode = 'w')
+    f = open('task-%s.txt'%second_argument, mode = 'w')
     f.write(now)
     f.close()
 
 if first_argument == 'pabaigti':
-    f = open('task.txt')
+    f = open('task-%s.txt'%second_argument)
     pradeti = f.read()
     print(pradeti)
     print(now)
@@ -21,4 +23,6 @@ if first_argument == 'pabaigti':
     now = datetime.datetime.strptime(now, '%Y-%m-%d %H:%M:%S.%f')
     pradeti = datetime.datetime.strptime(pradeti, '%Y-%m-%d %H:%M:%S.%f')
     print(now - pradeti)
-   
+    
+
+    #i stringa ideti papild reiksme
